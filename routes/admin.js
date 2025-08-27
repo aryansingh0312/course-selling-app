c4a21ec1038b33c572cda0e8b25b1d4c08db269c
aryansingh0312 <aryansingh265653@gmail.com>
2025-08-27 23:40:11 +0530

    Added Admmin routers

--- routes/admin.js
+++ routes/admin.js
@@ -62,14 +62,41 @@ adminRouter.post("course/",adminMiddleware, async function(req, res) {
         courseId: course._id
     })
 })
-adminRouter.put("course/", function(req, res) {
+adminRouter.put("/course",adminMiddleware, async function(req, res) {
+    const adminId = req.userId;
+
+    const {title, description, imageUrl, price, courseId} = req.body;
+
+    const course = await courseModel.updateOne({
+        _id: courseId,
+        creatorId: adminId
+    }, {
+        title: title,
+        description: description,
+        imageUrl:  imageUrl,
+        price: price,
+        creatorId: adminId
+    })
+
+    res.json({
+        message: "Course updated",
+        courseId: course._id
+    })
     res.json({
         message: "signup endpoint"
     })
 })
-adminRouter.get("course/bulk", function(req, res) {
+adminRouter.get("/course/bulk",adminMiddleware, async function(req, res) {
+    const adminId = req.userId;
+
+    const courses = await courseModel.find({
+        _id: courseId,
+        creatorId: adminId
+    });
+
     res.json({
-        message: "signup endpoint"
+        message: "Course updated",
+        courses
     })
 })
 
